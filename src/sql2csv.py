2b0a3f7bd34434c9b9d66b6a1898c3aa94df6161
gab <gabrielbordeaux@gmail.com>
2018-11-16 13:21:48 -0500

    Exporter

--- src/sql2csv.py
+++ src/sql2csv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import csv
+from os.path import expanduser
 
 import argparse
 import pymysql.cursors
@@ -20,7 +21,6 @@ def get_mysql_connection(host, user, port, password, database):
                            password=password,
                            db=database,
                            charset='utf8mb4',
-                           cursorclass=pymysql.cursors.DictCursor,
                            client_flag=pymysql.constants.CLIENT.MULTI_STATEMENTS
                            )
 
@@ -50,3 +50,125 @@ def get_connection(engine, host, user, port, password, database):
     else:
         raise RuntimeError(
             '"%s" engine is not supported.' % (engine))
+
+
+def get_cursor(connection):
+    """
+        Return connection cursor
+    """
+
+    return connection.cursor()
+
+
+def run_query(cursor, query):
+    """ Run a query and yield each row """
+
+    cursor.execute(query)
+
+    for row in cursor.fetchall():
+        yield row
+
+
+def resolve_home_dir(destination):
+    """ Resolve `~` to a full path """
+
+    if destination[:1] == '~':
+        return expanduser("~") + destination[1:]
+
+    return destination
+
+
+def open_file(destination):
+    """ Open file """
+
+    return open(destination, 'w', newline='')
+
+
+def get_writer(file_, delimiter=',', quotechar='"'):
+    """ Return a writer object """
+
+    return csv.writer(
+        file_,
+        delimiter=delimiter,
+        quotechar=quotechar, quoting=csv.QUOTE_MINIMAL
+    )
+
+
+def to_csv(engine, host, user, port, password, database, query, destination, delimiter=',', quotechar='"'):
+    """ Run a query and store the result to a CSV file """
+
+    # Get SQL connection
+    connection = get_connection(
+        engine=engine,
+        host=host,
+        user=user,
+        port=port,
+        password=password,
+        database=database
+    )
+    cursor = get_cursor(connection)
+
+    print('\n* Exporting rows...')
+
+    # Open CSV
+    file_ = open_file(resolve_home_dir(destination))
+    writer = get_writer(file_, delimiter=delimiter, quotechar=quotechar)
+
+    # Run query and write rows to CSV
+    i = 0
+    for row in run_query(cursor=cursor, query=query):
+        # Increment row counter
+        i += 1
+
+        if i % 100 == 0:
+            print('  ...%s rows written' % "{:,}".format(i))
+
+        writer.writerow(row)
+
+    print('  ...done')
+
+    print('* The result has been exported to %s.\n' % (destination))
+
+
+def main():
+    """ Parses arguments and run module """
+
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-e", "--engine", type=str, help="Database engine",
+                        choices=['mysql', 'postgresql'], default='mysql')
+    parser.add_argument("-H", "--host", default="127.0.0.1",
+                        help="Database host")
+    parser.add_argument("-P", "--port", type=int,
+                        default=3306, help="Database port")
+    parser.add_argument("-u", "--user", required=True, help="Database user")
+    parser.add_argument("-p", "--password", default='',
+                        help="Database password")
+    parser.add_argument("-d", "--database", required=True,
+                        help="Database name")
+    parser.add_argument("-q", "--query", required=True,
+                        help="SQL query")
+    parser.add_argument("-o", "--out",
+                        help="CSV destination", default='export.csv')
+    parser.add_argument("-D", "--delimiter", help="CSV delimiter", default=',')
+    parser.add_argument("-Q", "--quotechar",
+                        help="CSV quote character", default='"')
+    args = parser.parse_args()
+
+    to_csv(
+        engine=args.engine,
+        host=args.host,
+        user=args.user,
+        port=args.port,
+        password=args.password,
+        database=args.database,
+        query=args.query,
+        destination=args.out,
+        delimiter=args.delimiter,
+        quotechar=args.quotechar
+    )
+
+
+if __name__ == '__main__':
+    main()
--- src/unittest/test_sql2csv.py
+++ src/unittest/test_sql2csv.py
@@ -1,4 +1,6 @@
 import unittest
+import _io
+
 import psycopg2
 import pymysql
 
@@ -26,6 +28,21 @@ class Test(unittest.TestCase):
         }
     }
 
+    def get_connection(self, engine):
+
+        db_config = self.db_configs['pg' if engine ==
+                                    'postgresql' else 'mysql']
+
+        # Get database connection
+        return sql2csv.get_connection(
+            engine=engine,
+            host=db_config['host'],
+            user=db_config['user'],
+            port=db_config['port'],
+            password=db_config['password'],
+            database=db_config['db']
+        )
+
     def test_get_mysql_connection(self):
         db_config = self.db_configs['mysql']
 
@@ -33,8 +50,7 @@ class Test(unittest.TestCase):
         connection = sql2csv.get_mysql_connection(
             db_config['host'], db_config['user'], db_config['port'], db_config['password'], db_config['db'])
 
-        self.assertIsInstance(
-            connection, pymysql.connections.Connection)
+        self.assertIsInstance(connection, pymysql.connections.Connection)
 
     def test_get_pg_connection(self):
         db_config = self.db_configs['pg']
@@ -48,40 +64,23 @@ class Test(unittest.TestCase):
             database=db_config['db']
         )
 
-        self.assertIsInstance(
-            connection, psycopg2.extensions.connection)
+        self.assertIsInstance(connection, psycopg2.extensions.connection)
 
     def test_get_connection_engine_mysql(self):
-        db_config = self.db_configs['mysql']
-
         # Get database connection
-        connection = sql2csv.get_connection(
-            engine='mysql',
-            host=db_config['host'],
-            user=db_config['user'],
-            port=db_config['port'],
-            password=db_config['password'],
-            database=db_config['db']
+        connection = self.get_connection(
+            engine='mysql'
         )
 
-        self.assertIsInstance(
-            connection, psycopg2.extensions.connection)
+        self.assertIsInstance(connection, pymysql.connections.Connection)
 
     def test_get_connection_engine_postgresql(self):
-        db_config = self.db_configs['pg']
-
         # Get database connection
-        connection = sql2csv.get_connection(
-            engine='postgresql',
-            host=db_config['host'],
-            user=db_config['user'],
-            port=db_config['port'],
-            password=db_config['password'],
-            database=db_config['db']
+        connection = self.get_connection(
+            engine='postgresql'
         )
 
-        self.assertIsInstance(
-            connection, psycopg2.extensions.connection)
+        self.assertIsInstance(connection, psycopg2.extensions.connection)
 
     def test_get_connection_engine_invalid_engine(self):
         db_config = self.db_configs['pg']
@@ -96,3 +95,109 @@ class Test(unittest.TestCase):
             password=db_config['password'],
             database=db_config['db']
         )
+
+    def test_resolve_home_dir(self):
+        assert sql2csv.resolve_home_dir('/tmp/file') == '/tmp/file'
+        assert sql2csv.resolve_home_dir('~/file') == '/root/file'
+
+    def test_open_file(self):
+        self.assertIsInstance(sql2csv.open_file(
+            '/tmp/file1'), _io.TextIOWrapper)
+
+    def test_get_writer(self):
+        file_ = sql2csv.open_file('/tmp/file2')
+        writer = sql2csv.get_writer(file_)
+
+        self.assertIsInstance(writer, object)
+
+    def get_cursor_mysql(self):
+        # Get database connection
+        connection = self.get_connection(
+            engine='mysql'
+        )
+
+        cursor = sql2csv.get_cursor(connection)
+
+        self.assertIsInstance(cursor, pymysql.cursors.Cursor)
+
+    def get_cursor_postgresql(self):
+        # Get database connection
+        connection = self.get_connection(
+            engine='postgresql'
+        )
+
+        cursor = sql2csv.get_cursor(connection)
+
+        self.assertIsInstance(cursor, psycopg2.extensions.cursor)
+
+    def test_run_query_mysql(self):
+        # Get database connection
+        connection = self.get_connection(
+            engine='mysql'
+        )
+
+        cursor = sql2csv.get_cursor(connection)
+
+        for row in sql2csv.run_query(cursor=cursor, query='SELECT 1;'):
+            self.assertIsInstance(row, tuple)
+
+    def test_run_query_postgresql(self):
+        # Get database connection
+        connection = self.get_connection(
+            engine='postgresql'
+        )
+
+        cursor = sql2csv.get_cursor(connection)
+
+        for row in sql2csv.run_query(cursor=cursor, query='SELECT generate_series(1, 5);'):
+            self.assertIsInstance(row, tuple)
+
+    def test_to_csv_mysql(self):
+        db_config = self.db_configs['mysql']
+
+        dest_file = '/tmp/file3'
+
+        sql2csv.to_csv(
+            engine='mysql',
+            host=db_config['host'],
+            user=db_config['user'],
+            port=db_config['port'],
+            password=db_config['password'],
+            database=db_config['db'],
+            query='SELECT * FROM some_mysql_table',
+            destination=dest_file
+        )
+
+        # Read file
+        with open(dest_file, 'r') as content_file:
+            content = content_file.read()
+
+        assert content == """1,12,hello world,2018-12-01 12:23:12
+2,15,hello,2018-12-05 12:18:12
+3,18,world,2018-12-08 12:17:12
+"""
+
+    def test_to_csv_postgresql(self):
+        db_config = self.db_configs['pg']
+
+        dest_file = '/tmp/file4'
+
+        sql2csv.to_csv(
+            engine='postgresql',
+            host=db_config['host'],
+            user=db_config['user'],
+            port=db_config['port'],
+            password=db_config['password'],
+            database=db_config['db'],
+            query='SELECT * FROM some_pg_table',
+            destination=dest_file
+        )
+
+        # Read file
+        with open(dest_file, 'r') as content_file:
+            content = content_file.read()
+
+        assert content == """1,12,hello world,2018-12-01 12:23:12
+2,15,hello,2018-12-05 12:18:12
+3,18,world,2018-12-08 12:17:12
+"""
