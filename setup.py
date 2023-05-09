f069b282c29653338add77751d7b424c1dc5aef8
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2023-05-08 20:13:48 -0400

    Update codecov integration (#10)
    * Update codecov integration
    
    * Update setup pypandoc
    
    * Install pandoc and pypandoc
    
    * pep8 fix
    
    * pep8 fix

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert_file('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
+import pypandoc
 
 setup(
     name='sql2csv',
     version='1.3',
     description='Run MySQL and PostgreSQL queries and store result in CSV',
-    long_description=long_description,
+    long_description=pypandoc.convert_file('README.md', 'rst'),
     author='Gabriel Bordeaux',
     author_email='pypi@gab.lc',
     url='https://github.com/gabfl/sql2csv',
